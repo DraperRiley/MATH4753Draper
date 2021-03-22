508cbd2e42bc159ac58cce38958c01867b4dd621
DraperRiley <rileyldraper@gmail.com>
2021-03-21 23:19:53 -0500

    added fire dataset

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' Fire damage
+#'
+#' A data set collectted concerning home damage form fire
+#'
+#' @format A dta frame with 15 rows and 2 variables
+#'
+#' \describe{
+#' \item{DISTANCE}{distance from fire station in miles}
+#' \item{DAMAGE}{X 1000 dollars in damage}
+#' }
+#'
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
